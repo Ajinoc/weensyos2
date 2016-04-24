d0346577785644c1a56f54b2b21a13b799ad046a
Adam Matthew Jones <adamj@lnxsrv05.seas.ucla.edu>
2016-04-24 08:48:59 -0700

    Upload

---
+++ schedos-app.h
@@ -0,0 +1,131 @@
+#ifndef WEENSYOS_SCHEDOS_APP_H
+#define WEENSYOS_SCHEDOS_APP_H
+#include "schedos.h"
+
+/*****************************************************************************
+ * schedos-app.h
+ *
+ *   System call functions and constants used by SchedOS applications.
+ *
+ *****************************************************************************/
+
+
+// The number of times each application should run
+#define RUNCOUNT	320
+
+
+/*****************************************************************************
+ * sys_yield
+ *
+ *   Yield control of the CPU to the kernel, which will pick another
+ *   process to run.  (It might run this process again, depending on the
+ *   scheduling policy.)
+ *
+ *****************************************************************************/
+
+static inline void
+sys_yield(void)
+{
+	// We call a system call by causing an interrupt with the 'int'
+	// instruction.  In weensyos, the type of system call is indicated
+	// by the interrupt number -- here, INT_SYS_YIELD.
+	asm volatile("int %0\n"
+		     : : "i" (INT_SYS_YIELD)
+		     : "cc", "memory");
+}
+
+
+/*****************************************************************************
+ * sys_exit(status)
+ *
+ *   Exit the current process with exit status 'status'.
+ *
+ *****************************************************************************/
+
+static inline void sys_exit(int status) __attribute__ ((noreturn));
+static inline void
+sys_exit(int status)
+{
+	// Different system call, different interrupt number (INT_SYS_EXIT).
+	// This time, we also pass an argument to the system call.
+	// We do this by loading the argument into a known register; then
+	// the kernel can look up that register value to read the argument.
+	// Here, the status is loaded into register %eax.
+	// You can load other registers with similar syntax; specifically:
+	//	"a" = %eax, "b" = %ebx, "c" = %ecx, "d" = %edx,
+	//	"S" = %esi, "D" = %edi.
+	asm volatile("int %0\n"
+		     : : "i" (INT_SYS_EXIT),
+		         "a" (status)
+		     : "cc", "memory");
+    loop: goto loop; // Convince GCC that function truly does not return.
+}
+
+/*****************************************************************************
+ * sys_priority(priority)
+ *
+ *   IF YOU IMPLEMENT EXERCISE 4.A, NAME YOUR SYSTEM CALL sys_priority .
+ *
+ *****************************************************************************/
+
+static inline void
+sys_priority(int priority)
+{
+	asm volatile("int %0\n"
+	     : : "i" (INT_SYS_PRIORITY),
+	         "a" (priority)
+	     : "cc", "memory");
+}
+
+
+/*****************************************************************************
+ * sys_share(share)
+ *
+ *   IF YOU IMPLEMENT EXERCISE 4.B, NAME YOUR SYSTEM CALL sys_share .
+ *
+ *****************************************************************************/
+
+static inline void
+sys_share(int share)
+{
+	asm volatile("int %0\n"
+	     : : "i" (INT_SYS_SHARE),
+	         "a" (share)
+	     : "cc", "memory");
+}
+
+ /*****************************************************************************
+ * sys_print(c)
+ *
+ *   EXERCISE 6:
+ *   Atomically print to console, updating cursorpos as necessary
+ *
+ *****************************************************************************/
+
+static inline void
+sys_print(unsigned int c)
+{
+	asm volatile("int %0\n"
+	     : : "i" (INT_SYS_PRINT),
+	         "a" (c)
+	     : "cc", "memory");
+}
+
+ /*****************************************************************************
+ * sys_ticket(amt)
+ *
+ *   EXERCISE 7:
+ *   Set the amount of lottery tickets that a process holds
+ *
+ *****************************************************************************/
+
+static inline void
+sys_ticket(int amt)
+{
+	asm volatile("int %0\n"
+	     : : "i" (INT_SYS_TICKET),
+	         "a" (amt)
+	     : "cc", "memory");
+}
+
+#endif
---
+++ schedos-kern.c
@@ -0,0 +1,376 @@
+#include "schedos-kern.h"
+#include "x86.h"
+#include "lib.h"
+
+/*****************************************************************************
+ * schedos-kern
+ *
+ *   This is the schedos's kernel.
+ *   It sets up process descriptors for the 4 applications, then runs
+ *   them in some schedule.
+ *
+ *****************************************************************************/
+
+// The program loader loads 4 processes, starting at PROC1_START, allocating
+// 1 MB to each process.
+// Each process's stack grows down from the top of its memory space.
+// (But note that SchedOS processes, like MiniprocOS processes, are not fully
+// isolated: any process could modify any part of memory.)
+
+#define NPROCS		5
+#define PROC1_START	0x200000
+#define PROC_SIZE	0x100000
+
+// +---------+-----------------------+--------+---------------------+---------/
+// | Base    | Kernel         Kernel | Shared | App 0         App 0 | App 1
+// | Memory  | Code + Data     Stack | Data   | Code + Data   Stack | Code ...
+// +---------+-----------------------+--------+---------------------+---------/
+// 0x0    0x100000               0x198000 0x200000              0x300000
+//
+// The program loader puts each application's starting instruction pointer
+// at the very top of its stack.
+//
+// System-wide global variables shared among the kernel and the four
+// applications are stored in memory from 0x198000 to 0x200000.  Currently
+// there is just one variable there, 'cursorpos', which occupies the four
+// bytes of memory 0x198000-0x198003.  You can add more variables by defining
+// their addresses in schedos-symbols.ld; make sure they do not overlap!
+
+
+// A process descriptor for each process.
+// Note that proc_array[0] is never used.
+// The first application process descriptor is proc_array[1].
+static process_t proc_array[NPROCS];
+
+// A pointer to the currently running process.
+// This is kept up to date by the run() function, in mpos-x86.c.
+process_t *current;
+
+// The preferred scheduling algorithm.
+int scheduling_algorithm;
+
+// Seed values for Exercise 7
+unsigned long x, y, z, w, v;
+
+// UNCOMMENT THESE LINES IF YOU DO EXERCISE 4.A
+// Use these #defines to initialize your implementation.
+// Changing one of these lines should change the initialization.
+#define __PRIORITY_1__ 4
+#define __PRIORITY_2__ 3
+#define __PRIORITY_3__ 2
+#define __PRIORITY_4__ 1
+
+// UNCOMMENT THESE LINES IF YOU DO EXERCISE 4.B
+// Use these #defines to initialize your implementation.
+// Changing one of these lines should change the initialization.
+#define __SHARE_1__ 4
+#define __SHARE_2__ 3
+#define __SHARE_3__ 2
+#define __SHARE_4__ 1
+
+// Default ticket amounts for Exercise 7
+// Can be changed by using sys_ticket()
+#define __TICKET_1__ 100
+#define __TICKET_2__ 50
+#define __TICKET_3__ 25
+#define __TICKET_4__ 10
+
+// USE THESE VALUES FOR SETTING THE scheduling_algorithm VARIABLE.
+#define __EXERCISE_1__   0  // the initial algorithm
+#define __EXERCISE_2__   2  // strict priority scheduling (exercise 2)
+#define __EXERCISE_4A__ 41  // p_priority algorithm (exercise 4.a)
+#define __EXERCISE_4B__ 42  // p_share algorithm (exercise 4.b)
+#define __EXERCISE_7__   7  // any algorithm for exercise 7
+
+
+/*****************************************************************************
+ * start
+ *
+ *   Initialize the hardware and process descriptors, then run
+ *   the first process.
+ *
+ *****************************************************************************/
+
+void
+start(void)
+{
+	int i;
+
+	// Set up hardware (schedos-x86.c)
+	segments_init();
+	interrupt_controller_init(0);
+	console_clear();
+
+	// Initialize process descriptors as empty
+	memset(proc_array, 0, sizeof(proc_array));
+	for (i = 0; i < NPROCS; i++) {
+		proc_array[i].p_pid = i;
+		proc_array[i].p_state = P_EMPTY;
+	}
+
+	// Set up process descriptors (the proc_array[])
+	for (i = 1; i < NPROCS; i++) {
+		process_t *proc = &proc_array[i];
+		uint32_t stack_ptr = PROC1_START + i * PROC_SIZE;
+
+		// Initialize the process descriptor
+		special_registers_init(proc);
+
+		// Set ESP
+		proc->p_registers.reg_esp = stack_ptr;
+
+		// Load process and set EIP, based on ELF image
+		program_loader(i - 1, &proc->p_registers.reg_eip);
+
+		// Mark the process as runnable!
+		proc->p_state = P_RUNNABLE;
+	}
+
+	// Initialize the cursor-position shared variable to point to the
+	// console's first character (the upper left).
+	cursorpos = (uint16_t *) 0xB8000;
+
+	// Initialize the scheduling algorithm.
+	// USE THE FOLLOWING VALUES:
+	//    0 = the initial algorithm
+	//    2 = strict priority scheduling (exercise 2)
+	//   41 = p_priority algorithm (exercise 4.a)
+	//   42 = p_share algorithm (exercise 4.b)
+	//    7 = any algorithm that you may implement for exercise 7
+	// Use spec numbers instead!!! (From Piazza)
+	scheduling_algorithm = 0;
+
+	// Setup priorities for Exercise 4A
+	// Default is reverse order, can be set to anything though
+	// Can also be changed using sys_priority and macros in schedos_1(234).c
+	proc_array[1].p_priority = __PRIORITY_1__;
+	proc_array[2].p_priority = __PRIORITY_2__;
+	proc_array[3].p_priority = __PRIORITY_3__;
+	proc_array[4].p_priority = __PRIORITY_4__;
+
+	// Setup shares for Exercise 4B
+	// Default is descending proportions, can be set to anything though
+	// Can also be changed using sys_share and macros in schedos_1(234).c
+	proc_array[1].p_share = __SHARE_1__;
+	proc_array[2].p_share = __SHARE_2__;
+	proc_array[3].p_share = __SHARE_3__;
+	proc_array[4].p_share = __SHARE_4__;
+
+	// Extra Credit Exercise 7
+	// Set the default amount of tickets each process gets for lottery
+	// scheduling. Can also be set using the syscall sys_ticket()
+	proc_array[1].p_tickets = __TICKET_1__;
+	proc_array[2].p_tickets = __TICKET_2__;
+	proc_array[3].p_tickets = __TICKET_3__;
+	proc_array[4].p_tickets = __TICKET_4__;
+
+	// Set seed values for Exercise 7
+	x = (unsigned long) read_cycle_counter();
+	y = (unsigned long) read_cycle_counter();
+	z = (unsigned long) read_cycle_counter();
+	w = (unsigned long) read_cycle_counter();
+	v = (unsigned long) read_cycle_counter();
+
+	// Switch to the first process.
+	run(&proc_array[1]);
+
+	// Should never get here!
+	while (1)
+		/* do nothing */;
+}
+
+
+
+/*****************************************************************************
+ * interrupt
+ *
+ *   This is the weensy interrupt and system call handler.
+ *   The current handler handles 4 different system calls (two of which
+ *   do nothing), plus the clock interrupt.
+ *
+ *   Note that we will never receive clock interrupts while in the kernel.
+ *
+ *****************************************************************************/
+
+void
+interrupt(registers_t *reg)
+{
+	// Save the current process's register state
+	// into its process descriptor
+	current->p_registers = *reg;
+
+	switch (reg->reg_intno) {
+
+	case INT_SYS_YIELD:
+		// The 'sys_yield' system call asks the kernel to schedule
+		// the next process.
+		schedule();
+
+	case INT_SYS_EXIT:
+		// 'sys_exit' exits the current process: it is marked as
+		// non-runnable.
+		// The application stored its exit status in the %eax register
+		// before calling the system call.  The %eax register has
+		// changed by now, but we can read the application's value
+		// out of the 'reg' argument.
+		// (This shows you how to transfer arguments to system calls!)
+		current->p_state = P_ZOMBIE;
+		current->p_exit_status = reg->reg_eax;
+		schedule();
+
+	case INT_SYS_PRIORITY:
+		// 'sys_user*' are provided for your convenience, in case you
+		// want to add a system call.
+		/* Your code here (if you want). */
+		current->p_priority = reg->reg_eax;
+		run(current);
+
+	case INT_SYS_SHARE:
+		/* Your code here (if you want). */
+		current->p_share = reg->reg_eax;
+		run(current);
+
+	case INT_SYS_PRINT:
+		// atomically prints to console
+		*cursorpos++ = reg->reg_eax;
+		run(current);
+
+	case INT_SYS_TICKET:
+		current->p_tickets = reg->reg_eax;
+		run(current);
+
+	case INT_CLOCK:
+		// A clock interrupt occurred (so an application exhausted its
+		// time quantum).
+		// Switch to the next runnable process.
+		schedule();
+
+	default:
+		while (1)
+			/* do nothing */;
+
+	}
+}
+
+// PRNG for use in Exercise 7
+// See http://mathoverflow.net/questions/29494/pseudo-random-number-generation-algorithms
+unsigned long
+getRandom(void) 
+{
+	unsigned long t; 
+	t=(x^(x>>7));
+	x=y;
+	y=z;
+	z=w;
+	w=v; 
+	v=(v^(v<<6))^(t^(t<<13));
+	return (y+y+1)*v;
+} 
+
+
+/*****************************************************************************
+ * schedule
+ *
+ *   This is the weensy process scheduler.
+ *   It picks a runnable process, then context-switches to that process.
+ *   If there are no runnable processes, it spins forever.
+ *
+ *   This function implements multiple scheduling algorithms, depending on
+ *   the value of 'scheduling_algorithm'.  We've provided one; in the problem
+ *   set you will provide at least one more.
+ *
+ *****************************************************************************/
+
+void
+schedule(void)
+{
+	pid_t pid = current->p_pid;
+
+	if (scheduling_algorithm == 0)
+		while (1) {
+			pid = (pid + 1) % NPROCS;
+
+			// Run the selected process, but skip
+			// non-runnable processes.
+			// Note that the 'run' function does not return.
+			if (proc_array[pid].p_state == P_RUNNABLE)
+				run(&proc_array[pid]);
+		}
+
+	// From Piazza, use numbers from the spec instead of
+	// the constants defined at the top of this file.
+	// Using 1 instead of __EXERCISE_2__
+	else if (scheduling_algorithm == 1)
+		while(1) {
+			int i;
+			for (i = 1; i < NPROCS; i++) {
+				if (proc_array[i].p_state == P_RUNNABLE)
+					run(&proc_array[i]);
+			}
+		}
+
+	// Exercise 4A
+	else if (scheduling_algorithm == 2)
+		while (1) {
+			int i;
+			int min = 0x7fffffff;
+
+			for (i = 1; i < NPROCS; i++) {
+				// Don't run terminated processes
+				if (proc_array[i].p_state != P_RUNNABLE)
+					continue;
+
+				// Finding min
+				if (proc_array[i].p_priority < min)
+					min = proc_array[i].p_priority;
+			}
+
+			// Fetch next process to run
+			pid = (pid + 1) % NPROCS;
+			if (proc_array[pid].p_state == P_RUNNABLE && proc_array[pid].p_priority <= min)
+				run(&proc_array[pid]);
+		}
+
+	// Exercise 4B
+	else if (scheduling_algorithm == 3)
+		while (1) {
+			if (proc_array[pid].p_state == P_RUNNABLE) {
+				// End current run if over share
+				if (proc_array[pid].p_currentRun >= proc_array[pid].p_share) {
+					proc_array[pid].p_currentRun = 0;
+				} else {
+					proc_array[pid].p_currentRun++;
+					run(&proc_array[pid]);
+				}
+			}
+
+			// Loop through processes
+			pid = (pid + 1) % NPROCS;
+		}
+
+	// Exercise 7; lottery scheduling
+	else if (scheduling_algorithm == 4)
+		while (1) {
+			unsigned int i;
+			unsigned int current_amount = 0;
+			unsigned long total_tickets = 0;
+			unsigned long random;
+
+			for (i = 1; i < NPROCS; i++)
+				total_tickets += proc_array[i].p_tickets;
+
+			random = getRandom();
+	        random = random % total_tickets + 1;
+
+	        for (i = 1; i < NPROCS; i++) {
+	        	current_amount += proc_array[i].p_tickets;
+	        	if (current_amount >= random && proc_array[i].p_state == P_RUNNABLE)
+	        		run(&proc_array[i]);
+	        }
+		}
+
+	// If we get here, we are running an unknown scheduling algorithm.
+	cursorpos = console_printf(cursorpos, 0x100, "\nUnknown scheduling algorithm %d\n", scheduling_algorithm);
+	while (1)
+		/* do nothing */;
+}
